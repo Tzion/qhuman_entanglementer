b411fa8c9f572b4b7f2df89e81b9217079249b5a
zions <tzion12345@gmail.com>
2024-05-31 21:31:57 +0300

    receive keyboard events

--- event_bus.py
+++ event_bus.py
@@ -1,15 +1,22 @@
-import keyboard
 from abc import ABC, abstractmethod
+import keyboard
 from logger import log
+import pdb
+
+class Subscriber(ABC):
+    @abstractmethod
+    def handle_event(self, event):
+        pass
+
 
 class EventBus(ABC):
     def __init__(self):
         self._subscribers = []
 
-    def subscribe(self, subscriber):
+    def subscribe(self, subscriber: Subscriber):
         self._subscribers.append(subscriber)
 
-    def unsubscribe(self, subscriber):
+    def unsubscribe(self, subscriber:Subscriber):
         self._subscribers.remove(subscriber)
 
     def post(self, event):
@@ -17,18 +24,23 @@ class EventBus(ABC):
             subscriber.handle_event(event)
 
     @abstractmethod
-    def wait_for_event(self):
+    def wait_for_events(self):
         pass
+
 
 
 class GpioEventBus(EventBus):
-    def wait_for_event(self):
+    def wait_for_events(self):
         pass
 
 class KeyboardEventBus(EventBus):
-    def wait_for_event(self):
+    def wait_for_events(self):
         while True:
             event = keyboard.read_event()
-            log.info('Received keyboard event: %s', event)
+            log.info('Received keyboard event: %s: %s', event, event.__dict__)
+            if event.scan_code == 12:  # the buttun 'q'
+                event.type = 'contact'
+            if event.scan_code == 14:  # the button 'e'
+                event.type = 'explain'
             self.post(event) 
 
--- quantum_tunnel.py
+++ quantum_tunnel.py
@@ -1,19 +1,35 @@
-from event_bus import KeyboardEventBus
+from event_bus import KeyboardEventBus, Subscriber
 from logger import log
+import time
 
-class QuantumTunnel:
+class QuantumTunnel(Subscriber):
     def __init__(self):
         self.state = 'idle'
 
     def handle_event(self, event):
-        log.debug("QuantumTunnel received event: %s", event)
+        log.debug("QuantumTunnel received event of type %s: %s ", event.type if hasattr(event.type) else None, event)
+        if event.type == 'explain':
+            self.handle_explain_event() 
+        if event.type == 'contact':
+            self.handle_contact_event()
+
+    def handle_contact_event(self):
+        # Add your code here to handle the 'contact' event
+        pass
+
+    def handle_explain_event(self):
+        time.sleep(3)
+
+        
 
 
 def main():
     quantum_tunnel = QuantumTunnel()
     event_bus = KeyboardEventBus()
     event_bus.subscribe(quantum_tunnel)
-    event_bus.wait_for_event()
+    event_bus.wait_for_events()
 
 if __name__ == "__main__":
     main()
+
+import gtts
