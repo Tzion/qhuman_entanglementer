c49b87f77454a1548416b8dae0df2dcad1cee506
zions <tzion12345@gmail.com>
2024-06-02 14:44:31 +0300

    gpio pins

--- src/qhuman_entangler/tests/test_gpio.py
+++ src/qhuman_entangler/tests/test_gpio.py
@@ -4,6 +4,7 @@ GPIO.setmode(GPIO.BCM)
 gpio_pins = list(range(2, 28))  # Read all GPIO pins on Raspberry Pi
 
 def test_read_gpio_pins():
+    gpio_pins = [13]
     for pin in gpio_pins:
         GPIO.setup(pin, GPIO.IN)
 
@@ -19,3 +20,6 @@ def test_read_gpio_pins():
 
     except KeyboardInterrupt:
         GPIO.cleanup()
+
+if __name__ == "__main__":
+    test_read_gpio_pins()
