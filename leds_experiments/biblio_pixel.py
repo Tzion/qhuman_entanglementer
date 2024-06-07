ed73c6af2ddd9b27da3368ac92f07e096e73ed9a
Tzion <tzion12345@gmail.com>
2024-06-07 17:15:53 +0300

    using circle animation for pixelbible

--- leds_experiments/biblio_pixel.py
+++ leds_experiments/biblio_pixel.py
@@ -2,13 +2,21 @@ from BiblioPixelAnimations.simple.Fill import Fill
 from bibliopixel.drivers.PiWS281X import PiWS281X
 import BiblioPixelAnimations
 from bibliopixel import LEDStrip
+from BiblioPixelAnimations.circle.bloom import CircleBloom
+from bibliopixel import LEDCircle
 
 # Use the PWM interface on GPIO 18
-driver = PiWS281X(num=286, gpio=18)
+driver = PiWS281X(num=287, gpio=18)
 
-led = LEDStrip(driver)
-anim = Fill(led)
+#led = LEDStrip(driver)
+#anim = Fill(led)
+rings=[[0, 41], [41, 82], [82, 123], [123, 164], [164, 205], [205, 246] ,[246, 287]]
+led = LEDCircle(driver, rings=rings)
 
+anim = CircleBloom(led)
+
+
+import pdb;pdb.set_trace()
 try:
     anim.run()
 except KeyboardInterrupt:
