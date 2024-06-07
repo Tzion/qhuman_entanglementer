fedf78cbe779fc5f20450294cebbccdfd1d21f95
zions <tzion12345@gmail.com>
2024-06-07 14:37:13 +0300

    try with different driver

--- leds_experiments/biblio_pixel.py
+++ leds_experiments/biblio_pixel.py
@@ -1,9 +1,10 @@
 from bibliopixel.animation import StripChannelTest
-from bibliopixel.drivers.SPI import WS281X
+from bibliopixel.drivers.SPI.WS2801 import WS2801
 import BiblioPixelAnimations
 from bibliopixel import LEDStrip
 
-driver = WS281X(num=286, pin=18)
+# driver = WS281X(num=286, pin=18)
+driver = WS2801(num=286)
 led = LEDStrip(driver)
 anim = StripChannelTest(led)
 
