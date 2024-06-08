51df99117bba5598832802e3c466360289c20ad2
zions <tzion12345@gmail.com>
2024-06-08 10:22:20 +0300

    contact detection on the arduino side

--- src/qhuman_entangler/qhuman_entangler.ino
+++ src/qhuman_entangler/qhuman_entangler.ino
@@ -1,27 +1,44 @@
 int voltageSensorPin = A7;
 int voltageSensorValue = 0;
 
+const int numSamples = 46;
+const int initialVoltageEstimation = 533; // guessing the value to help reaching steady state faster
+int voltageSamples[numSamples] = {initialVoltageEstimation};  // using it as cyclic array, initialized with 0
+int sampleIndex = 0;
+const int outputPin = 9;
+
 void setup()
 {
   Serial.begin(9600);
+  digitalWrite(outputPin, LOW);
 }
 
 void loop()
 {
   voltageSensorValue = analogRead(voltageSensorPin);
-  Serial.print("Voltage measured: ");
+  Serial.print("Voltage measured of pin A7:");
   Serial.println(voltageSensorValue);
-  Serial.print("Pin A6: ");
-  Serial.println(analogRead(A6));
-  Serial.print("Pin A5: ");
-  Serial.println(analogRead(A5));
-  Serial.print("Pin A4: ");
-  Serial.println(analogRead(A4));
-  Serial.print("Pin A3: ");
-  Serial.println(analogRead(A3));
-  Serial.print("Pin A2: ");
-  Serial.println(analogRead(A2));
-  Serial.print("Pin A1: ");
-  Serial.println(analogRead(A1));
-  delay(1000);
+
+  voltageSamples[sampleIndex] = voltageSensorValue;
+  sampleIndex = (sampleIndex + 1) % numSamples;
+
+  int sum = 0;
+  for (int i = 0; i < numSamples; i++)
+  {
+    sum += voltageSamples[i];
+  }
+  int samplesAverage = sum / numSamples;
+  Serial.print("Average voltage: ");
+  Serial.println(samplesAverage);
+
+  if (voltageSensorValue < samplesAverage * .83) {
+    Serial.println("Voltage drop detected");
+    digitalWrite(outputPin, HIGH);
+  }
+  if (voltageSensorValue > samplesAverage * .92) {
+    digitalWrite(outputPin, LOW);
+  }
+  Serial.print("Output pin state: ");
+  Serial.println(digitalRead(outputPin) == HIGH ? "HIGH" : "LOW");
+  delay(500);
 }
