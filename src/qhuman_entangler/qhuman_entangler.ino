daa9a6d4b0321671b296bf64c28f5b12a071eb24
zions <tzion12345@gmail.com>
2024-06-08 10:34:41 +0300

    pause sampling when there's contant

--- src/qhuman_entangler/qhuman_entangler.ino
+++ src/qhuman_entangler/qhuman_entangler.ino
@@ -2,15 +2,20 @@ int voltageSensorPin = A7;
 int voltageSensorValue = 0;
 
 const int numSamples = 46;
-const int initialVoltageEstimation = 533; // guessing the value to help reaching steady state faster
-int voltageSamples[numSamples] = {initialVoltageEstimation};  // using it as cyclic array, initialized with 0
+const int initialVoltageEstimation = 533; // guessig the initial voltage (~2.4V)
+int voltageSamples[numSamples];  // using it as cyclic array, initialized with 0
 int sampleIndex = 0;
 const int outputPin = 9;
+bool pauseSampling = false;
 
 void setup()
 {
   Serial.begin(9600);
   digitalWrite(outputPin, LOW);
+  // initialize the samlpes with the estimated voltage to help reaching steady state faster
+  for (int i=0; i<numSamples; i++) {
+    voltageSamples[i] = initialVoltageEstimation;
+  }
 }
 
 void loop()
@@ -19,8 +24,11 @@ void loop()
   Serial.print("Voltage measured of pin A7:");
   Serial.println(voltageSensorValue);
 
+// when there's contact (low voltage) we want to keep the average value as a reference point
+if (!pauseSampling) {
   voltageSamples[sampleIndex] = voltageSensorValue;
   sampleIndex = (sampleIndex + 1) % numSamples;
+}
 
   int sum = 0;
   for (int i = 0; i < numSamples; i++)
@@ -34,9 +42,11 @@ void loop()
   if (voltageSensorValue < samplesAverage * .83) {
     Serial.println("Voltage drop detected");
     digitalWrite(outputPin, HIGH);
+    pauseSampling = true;
   }
   if (voltageSensorValue > samplesAverage * .92) {
     digitalWrite(outputPin, LOW);
+    pauseSampling = false;
   }
   Serial.print("Output pin state: ");
   Serial.println(digitalRead(outputPin) == HIGH ? "HIGH" : "LOW");
