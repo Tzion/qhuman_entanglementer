2c61254a659c8e46b80f942aa94fb12ddd707c3b
Tzion <tzion12345@gmail.com>
2024-06-09 05:34:35 +0300

    try mo

--- jobs/start-quantum-tunnel.sh
+++ jobs/start-quantum-tunnel.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sleep 30
+source ~/.bashrc
+sleep 60
 cd /home/zion/quanthuman-entangler && source venv_pi/bin/activate && python src/qhuman_entangler/quantum_tunnel.py >> /home/zion/quantum_tunnel.log 2>&1
