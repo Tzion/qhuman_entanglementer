6e167c4006180f6279ea533aa05e8bfc10f83d21
Tzion <tzion12345@gmail.com>
2024-06-10 19:32:32 +0300

    rerun test

---
+++ jobs/rerun-quantum-tunnel.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+while true
+do
+    sleep 30
+    echo "connect 04:FE:A1:8B:4C:14" | bluetoothctl
+    cd /home/zion/quanthuman-entangler 
+    source /home/zion/quanthuman-entangler/venv_pi/bin/activate 
+    python /home/zion/quanthuman-entangler/src/qhuman_entangler/quantum_tunnel.py > /home/zion/quantum_tunnel.log 2>&1 &
+    pid=$!
+    sleep 30
+    kill -9 $pid
+done
