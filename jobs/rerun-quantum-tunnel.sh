c95abbe6d051e91d7fb29ab581b811c4791a7a3f
Tzion <tzion12345@gmail.com>
2024-06-10 20:03:55 +0300

    adjust time

--- jobs/rerun-quantum-tunnel.sh
+++ jobs/rerun-quantum-tunnel.sh
@@ -2,12 +2,12 @@
 
 while true
 do
-    sleep 30
+    sleep 10
     echo "connect 04:FE:A1:8B:4C:14" | bluetoothctl
     cd /home/zion/quanthuman-entangler 
     source /home/zion/quanthuman-entangler/venv_pi/bin/activate 
     python /home/zion/quanthuman-entangler/src/qhuman_entangler/quantum_tunnel.py &
     pid=$!
-    sleep 30
+    sleep 9220
     kill -9 $pid
 done
