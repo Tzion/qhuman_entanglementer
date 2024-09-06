f15b8bdaf4448eb3a28aba7d0392387a79536ed4
zions <tzion12345@gmail.com>
2024-09-06 13:17:19 +0300

    improving sensing using median, wip

--- src/qhuman_entangler/qhuman_entangler.ino
+++ src/qhuman_entangler/qhuman_entangler.ino
@@ -1,12 +1,12 @@
 int voltageSensorPin = A7;
 int voltageSensorValue = 0;
 
-const int initialVoltageEstimation = 1000; // guessig the initial voltage (~5V)
+const int initialVoltageEstimation = 500; // 1024 is about 5V
 const int sampleFrequencyHz = 100;
 const int desiredActivationTime = 300; // in ms, this is actually the maximal time and the actual time will be half of it
 
 int delayMs = 50;
-const int numSamples = 600;
+const int numSamples = 400;
 int voltageSamples[numSamples]; // using it as cyclic array
 int sampleIndex = 0;
 const int logFreqMs = 2000; // log every 2 seconds
@@ -33,44 +33,59 @@ void initializeSamples(int voltage)
 void loop()
 {
   voltageSensorValue = analogRead(voltageSensorPin);
-
   if (voltageSensorValue == 0)
   {
-    // Serial.println("Voltage is 0 - wires may be disconnected - skipping measurement");
+    Serial.println("Voltage is 0 - wires may be disconnected - skipping measurement");
     digitalWrite(outputPin, LOW);
-    delay(delayMs); 
+    delay(delayMs);
     return;
   }
 
   collectSample(voltageSensorValue);
   logSamples(logFreqMs);
 
-  // Sort the samples
-  int sortedSamples[numSamples];
-  memcpy(sortedSamples, voltageSamples, sizeof(voltageSamples));
-  // std::sort(sortedSamples, sortedSamples + numSamples);
+  int medianVoltage = calcMedian();
+  print("Median voltage: %d", medianVoltage);
 
-  // Use the middle sample as the filtered value (median)
-  int filteredValue = sortedSamples[numSamples / 2];
-
-  // Serial.print("Filtered voltage (median): ");
-  // Serial.println(filteredValue);
-
-  if (voltageSensorValue < filteredValue * 0.8)
+  if (voltageSensorValue < medianVoltage * 0.72)
   {
-    // Serial.println("Voltage drop detected");
+    Serial.println("Voltage drop detected");
     digitalWrite(outputPin, HIGH);
     pauseSampling = true;
   }
-  if (voltageSensorValue > filteredValue * 0.4)
+  if (voltageSensorValue > medianVoltage * 0.8)
   {
     digitalWrite(outputPin, LOW);
     pauseSampling = false;
   }
 
-  // Serial.print("Output pin state: ");
+  // Serial.print("Output pin state ");
   // Serial.println(digitalRead(outputPin) == HIGH ? "HIGH" : "LOW");
   delay(delayMs); // Dynamic delay
+}
+
+int calcMedian()
+{
+  int sortedSamples[numSamples];
+  memcpy(sortedSamples, voltageSamples, sizeof(voltageSamples));
+  sort(sortedSamples, numSamples);
+  int median = sortedSamples[numSamples / 2];
+  return median;
+}
+void sort(int *array, int size)
+{
+  for (int i = 0; i < size - 1; i++)
+  {
+    for (int j = 0; j < size - i - 1; j++)
+    {
+      if (array[j] > array[j + 1])
+      {
+        int temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
 }
 
 void collectSample(int voltageSensorValue)
@@ -87,13 +102,8 @@ void logSamples(int logFreqMs)
 {
   if (sampleIndex * delayMs % logFreqMs == 0)
   {
-    int start = (sampleIndex - logFreqMs / delayMs + numSamples ) % numSamples;
-    printf("\n");
-    Serial.print("Samples (");
-    Serial.print(start);
-    Serial.print(" - ");
-    Serial.print(sampleIndex);
-    Serial.println("):");
+    int start = (sampleIndex - logFreqMs / delayMs + numSamples) % numSamples;
+    print("Samples (%d - %d):", start, sampleIndex);
     for (int i = start; i < sampleIndex; i++)
     {
       Serial.print(voltageSamples[i]);
@@ -101,4 +111,15 @@ void logSamples(int logFreqMs)
     }
     Serial.println();
   }
+}
+
+
+void print(const char *format, ...)
+{
+  char buffer[128];
+  va_list args;
+  va_start(args, format);
+  vsnprintf(buffer, sizeof(buffer), format, args);
+  va_end(args);
+  Serial.println(buffer);
 }
