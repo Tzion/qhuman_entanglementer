f836319ff8790c387a0c2416131ede5b78ca97d0
zions <tzion12345@gmail.com>
2024-09-09 17:12:16 +0300

    add jbl speaker

--- jobs/connect-bt-speaker.sh
+++ jobs/connect-bt-speaker.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-sleep 30
-echo "connect 04:FE:A1:8B:4C:14" | bluetoothctl
-
-
-[DEL] Device 5E:A6:EF:A9:36:98 LE-Beoplay EQ
-[NEW] Device 7B:F3:2C:89:39:AE LE-Beoplay EQ
+sleep 20
+# echo "connect 04:FE:A1:8B:4C:14" | bluetoothctl   # Home speakers
+echo "connect 78:44:05:9b:d2:70" | bluetoothctl # JBL Go (Green)
+echo "connect 30:C0:1B:65:66:EE" | bluetoothctl # JBL Go 2 (Blue)
